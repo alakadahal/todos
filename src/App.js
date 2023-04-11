4cc6af617b64700950186a02e0765de2c3769caf
Alaka Dahal <me@alakadahal.me>
2023-04-11 16:40:41 +0545

    added login form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Login from './Login';
 
-function App() {
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
-
-  const addTodo = (todo) => {
-    const newTodo = {
-      id: Math.random(),
-      todo: todo,
-    };
-    setList([...list, newTodo]); // adding todo to the list
-    setInput(""); //clearing todos
-  };
-
-  const deleteTodo = (id) => {
-    const newList = list.filter((todo) => todo.id !== id); //deleting todo items
-    setList(newList);
-  };
-
+const App = () => {
   return (
     <div>
-      <h1>Todo List</h1>
-      <input
-        type="text" value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => addTodo(input)}>Add</button>
-      <ol>
-        {list.map((todo) => (
-          <li key={todo.id}>
-            {todo.todo}
-            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
-          </li>
-        ))}
-      </ol>
+      <Login />
+
     </div>
-  );
+  )
 }
 
 export default App
---
+++ src/Login.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+
+function App() {
+
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+    const database = [
+        {
+            username: "admin",
+            password: "admin"
+        },
+        {
+            username: "root",
+            password: "root"
+        }
+    ];
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+    };
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+
+        const userData = database.find((user) => user.username === uname.value);
+
+
+        if (userData) {
+            if (userData.password !== pass.value) {
+
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+            }
+        } else {
+
+            setErrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div>{errorMessages.message}</div>
+        );
+
+
+    const renderForm = (
+        <div >
+            <form onSubmit={handleSubmit}>
+                <div >
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div >
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div >
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
+    );
+
+    return (
+        <div>
+            <div >
+                <div >Sign In</div>
+                {isSubmitted ? <div> Login sucessful
+                </div> : renderForm}
+            </div>
+        </div>
+    );
+}
+
+export default App;
